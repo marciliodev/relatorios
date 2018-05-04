57f142676033aace7c567da04115e629a641fad2
Marcilio <marcilio.pa@hotmail.com>
2018-05-04 17:05:49 -0300

    Janela Nova

--- application/controllers/PDF_c.php
+++ application/controllers/PDF_c.php
@@ -13,6 +13,142 @@ class PDF_c extends CI_Controller {
 
 	public function index()
 	{
+        $dados = array (
+            'escritorio' => $this->relacao_escritorio(),
+            'almoxarifado' => $this->relacao_almoxarifado(),
+            'servico_vascular' => $this->relacao_servico_vascular(),
+            'entrada' => $this->relacao_entrada()
+        );
+        $this->load->view('index', $dados);
+        //$this->load->view('comuns/body', [], TRUE);
+        //$this->load->view('comuns/footer');
+
+        //$this->relatorio_escritorio();
+        //$this->relatorio_entrada();
+        //$this->relatorio_escritorio();
+        //$this->relatorio_servico_vascular();
+    }
+
+    public function relacao_escritorio()
+    {
+        $dados['dados'] = $this->pdf->busca_produtos();
+        //var_dump($var);return;
+        return $this->load->view('pdf/material_escritorio/body', $dados, TRUE);
+    }
+
+    public function relacao_almoxarifado()
+    {
+        $dados['dados'] = $this->pdf->busca_produtos();
+        //var_dump($var);return;
+        return $this->load->view('pdf/material_almoxarifado/body', $dados, TRUE);
+    }
+
+    public function relacao_servico_vascular()
+    {
+        $dados['dados'] = $this->pdf->busca_produtos();
+        //var_dump($var);return;
+        return $this->load->view('pdf/material_servico_vascular/body', $dados, TRUE);
+    }
+
+    public function relacao_entrada()
+    {
+        $dados['dados'] = $this->pdf->busca_produtos();
+        //var_dump($var);return;
+        return $this->load->view('pdf/material_entrada/body', $dados, TRUE);
+    }
+
+    public function relatorio_escritorio()
+    {
+        $data = date('d/m/Y');
+        $dados['dados'] = $this->pdf->busca_produtos();
+        //var_dump($var);return;
+        $header = $this->load->view('pdf/material_escritorio/header', [], TRUE);
+        $html = $this->load->view('pdf/material_escritorio/body', $dados, TRUE);
+        //$footer = $this->load->view('pdf/material_escritorio/footer_material_escritorio', [], TRUE);
+        set_time_limit(300); //seta o tempo limite de resposta para 
+        ini_set("memory_limit", "600M"); //seta a quantidade de memória que pode ser usada pelo servidor
+        $mpdf = new \Mpdf\Mpdf([
+            'tempDir' => '/tmp', 
+            'setAutoTopMargin' => 'stretch', 
+            'setAutoBottomMargin' => 'stretch',
+            'autoMarginPadding' => -4,
+            'margin_left' => 5,
+            'margin_right' => 5,
+            'margin_bottom' => 10,
+            'margin_header' => 0,
+            'margin_footer' => 5,
+            'orientation' => 'L', 
+            'default_font' => 'arial'
+        ]);
+        $mpdf->SetTitle('Relatório de Materiais de Escritório');
+        $mpdf->SetHTMLHeader($header);
+        $mpdf->SetHTMLFooter('<b>Página {PAGENO}</br>');
+        $mpdf->WriteHTML($html);
+        return $mpdf->Output('Relatorio_Material_Escritório.pdf', I);
+    }
+
+    public function relatorio_almoxarifado()
+    {
+        $data = date('d/m/Y');
+        $dados['dados'] = $this->pdf->busca_produtos();
+        //var_dump($var);return;
+        $header = $this->load->view('pdf/material_almoxarifado/header', [], TRUE);
+        $html = $this->load->view('pdf/material_almoxarifado/body', $dados, TRUE);
+        //$footer = $this->load->view('pdf/material_escritorio/footer_material_escritorio', [], TRUE);
+        set_time_limit(300); //seta o tempo limite de resposta para 
+        ini_set("memory_limit", "600M"); //seta a quantidade de memória que pode ser usada pelo servidor
+        $mpdf = new \Mpdf\Mpdf([
+            'tempDir' => '/tmp', 
+            'setAutoTopMargin' => 'stretch', 
+            'setAutoBottomMargin' => 'stretch',
+            'autoMarginPadding' => -4,
+            'margin_left' => 5,
+            'margin_right' => 5,
+            'margin_bottom' => 10,
+            'margin_header' => 0,
+            'margin_footer' => 5,
+            'orientation' => 'L', 
+            'default_font' => 'arial'
+        ]);
+        $mpdf->SetTitle('Relatório de Materiais do Almoxarifado');
+        $mpdf->SetHTMLHeader($header);
+        $mpdf->SetHTMLFooter('<b>Página {PAGENO}</br>');
+        $mpdf->WriteHTML($html);
+        return $mpdf->Output('Relatorio_Material_Almoxarifado.pdf', I);
+    }
+
+    public function relatorio_servico_vascular()
+    {
+        $data = date('d/m/Y');
+        $dados['dados'] = $this->pdf->busca_produtos();
+        //var_dump($var);return;
+        $header = $this->load->view('pdf/material_servico_vascular/header', [], TRUE);
+        $html = $this->load->view('pdf/material_servico_vascular/body', $dados, TRUE);
+        //$footer = $this->load->view('pdf/material_escritorio/footer_material_escritorio', [], TRUE);
+        set_time_limit(300); //seta o tempo limite de resposta para 
+        ini_set("memory_limit", "600M"); //seta a quantidade de memória que pode ser usada pelo servidor
+        $mpdf = new \Mpdf\Mpdf([
+            'tempDir' => '/tmp', 
+            'setAutoTopMargin' => 'stretch', 
+            'setAutoBottomMargin' => 'stretch',
+            'autoMarginPadding' => -4,
+            'margin_left' => 5,
+            'margin_right' => 5,
+            'margin_bottom' => 10,
+            'margin_header' => 0,
+            'margin_footer' => 5,
+            'orientation' => 'L', 
+            'default_font' => 'arial'
+        ]);
+        $mpdf->SetTitle('Relatório de Materiais de Serviço Vascular');
+        $mpdf->SetHTMLHeader($header);
+        $mpdf->SetHTMLFooter('<b>Página {PAGENO}</br>');
+        $mpdf->WriteHTML($html);
+        return $mpdf->Output('Relatorio_Material_Serviço_Vascular.pdf', I);
+    }
+
+    public function relatorio_entrada()
+    {
         $data = date('d/m/Y');
         $dados['dados'] = $this->pdf->busca_produtos();
         //var_dump($var);return;
@@ -38,6 +174,6 @@ class PDF_c extends CI_Controller {
         $mpdf->SetHTMLHeader($header);
         $mpdf->SetHTMLFooter('<b>Página {PAGENO}</br>');
         $mpdf->WriteHTML($html);
-        $mpdf->Output('Relatorio_Material_Escritório.pdf', I);
+        return $mpdf->Output('Relatorio_Material_Entrada.pdf', I);
     }
 }
--- application/views/index.php
+++ application/views/index.php
@@ -1,35 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Relatórios do Almoxarifado</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Almoxarifado</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Relatórios<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Material de Escritório</a></li>
-          <li><a href="#">Material do Almoxarifado</a></li>
-          <li><a href="#">Material de Serviço Vascular</a></li>
-          <li><a href="#">Material de Entrada</a></li>
-        </ul>
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Relatórios Almoxarifado</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    </ul>
   </div>
 </nav>
-  
-<div class="container">
-  <h3>Relatórios do Almoxarifado</h3>
-  <?= $this->load->view('welcome_message.php', [], TRUE);?>
-</div>
 
+<div class="row">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action list-group-item-info" id="escritorio" data-toggle="list" href="#material_escritorio" role="tab" aria-controls="material_escritorio">Material de Escritório</a>
+      <a class="list-group-item list-group-item-action list-group-item-info" id="almoxarifado" data-toggle="list" href="#material_almoxarifado" role="tab" aria-controls="material_almoxarifado">Material do Almoxarifado</a>
+      <a class="list-group-item list-group-item-action list-group-item-info" id="servico_vascular" data-toggle="list" href="#material_servico_vascular" role="tab" aria-controls="material_servico_vascular">Material de Serviço Vascular</a>
+      <a class="list-group-item list-group-item-action list-group-item-info" id="entrada" data-toggle="list" href="#material_entrada" role="tab" aria-controls="material_entrada">Material de Entrada</a>
+    </div>
+    
+      <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="material_escritorio" role="tabpanel" aria-labelledby="material_escritorio">
+      <input class="btn btn-primary" type="submit" value="Imprimir" name="imprimir">
+      <?=$escritorio?>
+      </div>
+
+      <div class="tab-pane fade" id="material_almoxarifado" role="tabpanel" aria-labelledby="material_almoxarifado">
+        <?=$almoxarifado?>
+        </div>
+      <div class="tab-pane fade" id="material_servico_vascular" role="tabpanel" aria-labelledby="material_servico_vascular">
+        <?=$servico_vascular?>
+      </div>
+
+      <div class="tab-pane fade" id="material_entrada" role="tabpanel" aria-labelledby="material_entrada">
+        <?=$entrada?></div>
+    </div>
+</div>
 </body>
 </html>
