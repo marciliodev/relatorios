e3bdebf5e59a35ecf6f65859173e869dbfea74ee
Marcílio Aguiar <marcilio.pa@hotmail.com>
2018-07-23 12:47:52 -0300

    Versão 2.0.1

--- application/views/index.php
+++ application/views/index.php
@@ -14,9 +14,9 @@
 
 <script src="<?=('assets/js/jquery.min.js') ?>"></script>
 <script src="<?=('assets/js/jquery-3.3.1.min.js') ?>"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="<?=('assets/js/jquery-3.3.1.slim.min.js') ?>"></script>
+<script src="<?=('assets/js/popper.min.js') ?>"></script>
+<script src="<?=('assets/js/bootstrap-4.1.1.min.js') ?>"></script>
 <script src="<?=('assets/js/bootstrap-datepicker.min.js') ?>"></script>
 <script src="<?=('assets/js/funcoes.js') ?>"></script>
 
